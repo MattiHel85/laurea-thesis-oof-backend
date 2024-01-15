1ab2f13a7b0df83c9f8e0001c5004258e7ad7039
Matt Simpson <70198997+MattiHel85@users.noreply.github.com>
2024-01-15 07:51:31 +0200

    Create & test user routes w/ passport & authUtils

---
+++ src/app.ts
@@ -0,0 +1,53 @@
+require('dotenv').config();
+import express from 'express';
+
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 3000;
+const mongoose = require('mongoose');
+const mongoPass = 'QG26uJQnUAZL1n2G';
+
+// Parse JSON and URL-encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// import routes 
+
+const userRoutes = require('./routes/userRoutes');
+
+// db connection address
+const uri = `mongodb+srv://mattrcsimpson:${mongoPass}@cluster0.6lvmky3.mongodb.net/?retryWrites=true&w=majority`;
+// connect to db
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const database = mongoose.connection;
+
+database.on("error", (err: Error) => console.log(`Connection Error: ${err}`));
+database.once("open", () => console.log("Welcome to the OOF database!"));
+
+app.get('/', (req, res) => {
+  res.send('Hello, Oof!');
+});
+
+/* USER ROUTES */
+
+// get users 
+app.get('/users', userRoutes.getAllUsers);
+// add user
+app.post('/users', userRoutes.addUser);
+// get user by id
+app.get('/users/:id', userRoutes.getUserById)
+// update user
+app.patch('/users/:id', userRoutes.updateUserById);
+// delete user
+app.delete('/users/:id', userRoutes.deleteUserById);
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,1 +1,35 @@
-// passport config goes here
+import { 
+    Strategy, 
+    ExtractJwt, 
+    StrategyOptions,
+    VerifiedCallback
+} from 'passport-jwt';
+import passport from 'passport';
+import { JwtPayload } from '../types/user';
+const UserModel = require('../models/user');
+
+const jwtOptions: StrategyOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET_KEY || 'askOchEmbla',
+};
+
+passport.use(
+    new Strategy(jwtOptions, async (payload: JwtPayload, done: VerifiedCallback) => {
+        try {
+            const user = await UserModel.findById(payload.id);
+            
+            if(user){
+                console.log('User found:', user);
+                return done(null, user);
+            } else {
+                console.log('User not found');
+                return done(null, false)
+            }
+        } catch (error){
+            console.error('Error during authentication:', error);
+            return done(error, false)
+        }
+    })
+)
+
+export default passport;
